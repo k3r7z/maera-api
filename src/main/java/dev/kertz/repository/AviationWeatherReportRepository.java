f1bd0aa7530701979fced4b59b39302596e124d1
k3r7z <k3r7z@protonmail.com>
2025-07-27 09:35:47 -0300

    Changes in the repositories to match the model schema

--- src/main/java/dev/kertz/repository/AviationWeatherReportRepository.java
+++ src/main/java/dev/kertz/repository/AviationWeatherReportRepository.java
@@ -3,4 +3,4 @@ package dev.kertz.repository;
 import dev.kertz.model.AviationWeatherReport;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface AviationWeatherReportRepository extends JpaRepository<AviationWeatherReport, Integer> { }
+public interface AviationWeatherReportRepository extends JpaRepository<AviationWeatherReport, Integer> { }
--- src/main/java/dev/kertz/repository/ProvinceRepository.java
+++ src/main/java/dev/kertz/repository/ProvinceRepository.java
@@ -3,4 +3,4 @@ package dev.kertz.repository;
 import dev.kertz.model.Province;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProvinceRepository extends JpaRepository<Province, String> { }
+public interface ProvinceRepository extends JpaRepository<Province, Integer> { }
