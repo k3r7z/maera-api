52904ca90b52d7761bd8cb3db499193dcdbae191
k3r7z <k3r7z@protonmail.com>
2025-07-27 08:59:47 -0300

    Added id and constructor for Province model

--- src/main/java/dev/kertz/model/Province.java
+++ src/main/java/dev/kertz/model/Province.java
@@ -1,12 +1,21 @@
 package dev.kertz.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import lombok.Getter;
+import lombok.Data;
 
 @Entity
-@Getter
+@Data
 public class Province {
+
     @Id
+    private int id;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
+    public Province(String name){
+        this.name = name;
+    }
 }
