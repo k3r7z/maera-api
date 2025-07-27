691ed050980240f23f4d975445c1e96cfaa2759f
k3r7z <k3r7z@protonmail.com>
2025-07-27 09:35:11 -0300

    Changes in the model schema

--- src/main/java/dev/kertz/model/AviationWeatherReport.java
+++ src/main/java/dev/kertz/model/AviationWeatherReport.java
@@ -2,13 +2,17 @@ package dev.kertz.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class AviationWeatherReport {
     @Id
+    @GeneratedValue
     private int id;
 
     @Column(length = 50)
@@ -16,4 +20,9 @@ public class AviationWeatherReport {
 
     @Column
     private String url;
+
+    public AviationWeatherReport(String name, String url) {
+        this.name = name;
+        this.url = url;
+    }
 }
--- src/main/java/dev/kertz/model/Province.java
+++ src/main/java/dev/kertz/model/Province.java
@@ -4,9 +4,11 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
 public class Province {
 
     @Id
